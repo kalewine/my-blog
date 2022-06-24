88dd514f4eb68056009a9b80ffe6f27d9220106e
kalewine <kayla.r.alewine@gmail.com>
2022-06-24 17:58:08 -0400

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    contentApiKey: '511a562ea78c0a6c0e2121a00f', 
+    blogUrl: 'kayla-alewines-blog.ghost.io'
+  }
 }
 
 module.exports = nextConfig
