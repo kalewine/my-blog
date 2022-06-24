b34152956785eb6bb4c73e8fe889744d7e73b530
kalewine <kayla.r.alewine@gmail.com>
2022-06-24 18:04:59 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    contentApiKey: '511a562ea78c0a6c0e2121a00f', 
+    contentApiKey: 'testing1-2-3', 
     blogUrl: 'kayla-alewines-blog.ghost.io'
   }
 }
