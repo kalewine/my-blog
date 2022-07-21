c27e86e5ef2c19c92eceebfee7afab530098fe6c
kalewine <kayla.r.alewine@gmail.com>
2022-07-21 04:11:23 -0400

    Remove console.log

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,6 @@ const getPosts = async () => {
   const res = await fetch(`https://www.googleapis.com/blogger/v3/blogs/${process.env.BLOG_ID}/posts?key=${process.env.KEY}`).then((res) => res.json() )
 
   const posts = res.items.map(post => post)
-  console.log(posts)
   return {posts}
 }
 
