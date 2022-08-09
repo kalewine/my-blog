65ec84bb9b7b39201027db4db18b6eec1b1e53d1
kalewine <kayla.r.alewine@gmail.com>
2022-08-09 15:12:28 -0400

    Trigger deploy when blog is updated

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,7 +1,20 @@
+let oldPostNum; 
 
 export const getPosts = async () => {
     const res = await fetch(`https://www.googleapis.com/blogger/v3/blogs/${process.env.BLOG_ID}/posts?key=${process.env.KEY}`).then((res) => res.json() )
-  
+    let postNum = res.items.length;
     const posts = res.items.map(post => post)
+    if(postNum !== oldPostNum){
+        console.log(postNum)
+        deploy()
+        oldPostNum = postNum;
+    }
     return {posts}
-  }
+   
+}
+
+
+let deploy = async() => {
+    console.log('trigger deploy')
+    const res = await fetch(`https://api.netlify.com/build_hooks/${process.env.DEPLOY}`)
+}
